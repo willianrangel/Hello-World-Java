114bb752a62a331ee50143a99db10a39b5c0a279
javalisson <javalisson@gmail.com>
2017-02-21 11:12:14 -0300

    Adicionado a classe principal HelloWorld
    Também foram criadas as pastas de fonte e de executáveis.

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,24 @@
+/**
+ * Arquivo: HelloWorld/src/helloworld/HelloWorld.java
+ */
+
+package helloworld;
+
+/**
+ * Um programa que diz Ola Mundo
+ *
+ * Instrucoes:
+ * Abra o CMD e acesse a pasta HelloWorld, que contem as pastas
+ * src (dos fontes) bin (os executaveis) e digite os comandos abaixo:
+ *
+ * PARA COMPILAR:
+ * javac -d bin src\helloworld\HelloWorld.java
+ *
+ * PARA EXECUTAR:
+ * java -cp bin helloworld.HelloWorld
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Ola, Mundo");
+	}
+}
