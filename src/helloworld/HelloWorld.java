563f3f1f992e383b9176c14363d700f0d0346fbc
willianrangel <wiawiaster@gmail.com>
2017-03-17 20:35:04 -0300

    Adicionado argumentos para exibir o nome

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,22 +3,12 @@
  */
 
 package helloworld;
-
-/**
- * Um programa que diz Ola Mundo
- *
- * Instrucoes:
- * Abra o CMD e acesse a pasta HelloWorld, que contem as pastas
- * src (dos fontes) bin (os executaveis) e digite os comandos abaixo:
- *
- * PARA COMPILAR:
- * javac -d bin src\helloworld\HelloWorld.java
- *
- * PARA EXECUTAR:
- * java -cp bin helloworld.HelloWorld
- */
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Ola, Mundo");
+		if(args.length > 0){
+			System.out.println("Ola, " + args[0]);
+		}else{
+			System.out.println("Ola, Mundo");
+		}
 	}
 }
